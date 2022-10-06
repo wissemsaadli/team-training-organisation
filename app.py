b67b5c34a54c736c5c89f6fd547bf89e9f256154
wissem <saadliwissem88@gmail.com>
2022-10-06 11:18:20 +0100

    till 21

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
+# importing flask and a template render
+
 from flask import Flask , render_template
+# importing data base
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime, func
 app = Flask(__name__)
+# initializing a database
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+ 
+db=SQLAlchemy(app)
+app.app_context().push()
+# importing date time
+import datetime
+
+
+class ToDo(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    content=db.Column(db.String(20), nullable=False)
+    time_created = db.Column(DateTime(timezone=True), server_default=func.now())
+
+    def __rep__(self):
+        return '<Task %r>' %self.id
+
+        
 @app.route('/')
 def index():
     return render_template("index.html")
